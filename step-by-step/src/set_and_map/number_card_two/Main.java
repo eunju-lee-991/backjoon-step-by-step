823b23b4842d6fca15ddf690753c40ef66ec1430
eunju <eunju7757@kai-i.com>
2024-06-18 16:41:01 +0900

    solve number card 2

--- step-by-step/src/set_and_map/number_card_two/Main.java
+++ step-by-step/src/set_and_map/number_card_two/Main.java
@@ -1,18 +1,25 @@
 package set_and_map.number_card_two;
 
 import java.io.*;
-import java.util.Comparator;
-import java.util.SortedSet;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
+// 10816 숫자 카드 2
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.merge(tokenizer.nextToken(), 1, Integer::sum);
+        }
+        n = Integer.parseInt(br.readLine());
+        tokenizer = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            bw.write(map.getOrDefault(tokenizer.nextToken(), 0) + " ");
+        }
         br.close();
-
 
         bw.flush();
         bw.close();
