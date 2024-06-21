6f0fa9f0431f356eafa59a033b55107bbf272959
eunju <eunju7757@kai-i.com>
2024-06-21 13:23:37 +0900

    solve queue 2 problem

---
+++ step-by-step/src/stack_and_queue_and_deck/josephus/Main.java
@@ -0,0 +1,20 @@
+package stack_and_queue_and_deck.josephus;
+
+import java.io.*;
+import java.util.Stack;
+
+// 10773 zero
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+
+        br.close();
+        bw.write(String.valueOf(123));
+
+        bw.flush();
+        bw.close();
+    }
+}
--- step-by-step/src/stack_and_queue_and_deck/queue_two/Main.java
+++ step-by-step/src/stack_and_queue_and_deck/queue_two/Main.java
@@ -1,6 +1,8 @@
 package stack_and_queue_and_deck.queue_two;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 // 10773 zero
@@ -9,10 +11,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-
+        Queue<String> queue = new LinkedList<>();
+        String last = "";
+        for (int i = 0; i < n; i++) {
+            String inst = br.readLine();
+            String result = "";
+            if (inst.startsWith("push")) {
+                last = inst.substring(5);
+                queue.offer(last);
+            } else {
+                switch (inst) {
+                    case "pop":
+                        result = queue.isEmpty() ? "-1" : queue.poll();
+                        break;
+                    case "size":
+                        result = String.valueOf(queue.size());
+                        break;
+                    case "empty":
+                        result = queue.isEmpty() ? "1" : "0";
+                        break;
+                    case "front":
+                        result = queue.isEmpty() ? "-1" : queue.peek();
+                        break;
+                    case "back":
+                        result = queue.isEmpty() ? "-1" : last;
+                        break;
+                }
+                bw.write(result + "\n");
+            }
+        }
         br.close();
-
-        bw.write(String.valueOf(123));
 
         bw.flush();
         bw.close();
