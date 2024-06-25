50ae417dead9df190c940bc1419fa359f60485ee
eunju <eunju7757@gmail.com>
2024-06-25 22:33:59 +0900

    solve 24511 queuestack

--- step-by-step/src/stack_and_queue_and_deq/queuestack/Main.java
+++ step-by-step/src/stack_and_queue_and_deq/queuestack/Main.java
@@ -1,42 +1,31 @@
 package stack_and_queue_and_deq.queuestack;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // 24511 queuestack
 public class Main {
-    static int[] dataStructures;
-    static int[] data;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        dataStructures = new int[n];
-        data = new int[n];
+        Deque<Integer> deque = new ArrayDeque<>();
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            dataStructures[i] = Integer.parseInt(tokenizer.nextToken());
-        } // 자료구조 담기
+        StringTokenizer tokenizer2 = new StringTokenizer(br.readLine());
 
-        tokenizer = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            data[i] = Integer.parseInt(tokenizer.nextToken());
-        } // 자료구조 초기값 담기
+            String value = tokenizer2.nextToken();
+            if (tokenizer.nextToken().equals("0")) {
+                deque.add(Integer.parseInt(value));
+            }
+        }
 
         n = Integer.parseInt(br.readLine());
         tokenizer = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             int input = Integer.parseInt(tokenizer.nextToken());
-            for (int j = 0; j < dataStructures.length; j++) {
-                if (dataStructures[j] == 0) { // queue
-                    int temp = data[j];
-                    data[j] = input;
-                    input = temp;
-                }
-            }
-            bw.write(input + " ");
+            deque.offerFirst(input);
+            bw.write(deque.pollLast() + " ");
         }
 
         br.close();
@@ -44,5 +33,4 @@ public class Main {
         bw.flush();
         bw.close();
     }
-
 }
