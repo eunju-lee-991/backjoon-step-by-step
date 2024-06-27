540ad14ef8f75dc5b57b5d42c43579b0c8195d8e
eunju <eunju7757@kai-i.com>
2024-06-27 10:50:41 +0900

    solve 9461 - padovan sequence

--- step-by-step/src/dynamic_programming/padovan_sequence/Main.java
+++ step-by-step/src/dynamic_programming/padovan_sequence/Main.java
@@ -2,17 +2,34 @@ package dynamic_programming.padovan_sequence;
 
 import java.io.*;
 
-// 1904 - 01타일
+// 9461 - 파도반 수열
 public class Main {
+    static long[] arr = new long[101];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(br.readLine());
+            bw.write(padovan(m) + "\n");
+        }
         br.close();
-
-        bw.write( " ");
-
         bw.flush();
         bw.close();
+    }
+
+    static long padovan(int m) {
+        for (int i = 1; i < m + 1; i++) {
+            if (arr[i] == 0) {
+                if (i < 4) {
+                    arr[i] = 1;
+                } else {
+                    arr[i] = arr[i - 2] + arr[i - 3];
+                }
+            }
+        }
+        return arr[m];
     }
 }
