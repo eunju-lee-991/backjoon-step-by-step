44c1073895e9ab93627bbced83abbe5d8c9f71de
eunju <eunju7757@kai-i.com>
2024-07-18 16:07:22 +0900

    solve 11053 - longest increasing subsequence

---
+++ step-by-step/src/dynamic_programming/longest_increasing_subsequence/Main.java
@@ -0,0 +1,34 @@
+package dynamic_programming.longest_increasing_subsequence;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 11053 - 가장 긴 증가하는 부분 수열
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] values = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        br.close();
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(values[j] < values[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        bw.write(String.valueOf(Arrays.stream(dp).max().getAsInt()));
+
+        bw.flush();
+        bw.close();
+    }
+}
