8f885f5b0ed548d5c711ab91ae9429589850b5bb
eunju <eunju7757@kai-i.com>
2024-09-20 09:29:43 +0900

    solve 2110 - router setting

--- step-by-step/src/binary_search/router_setting/Main.java
+++ step-by-step/src/binary_search/router_setting/Main.java
@@ -1,48 +1,46 @@
-package binary_search.wood_cutting;
+package binary_search.router_setting;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 2805 - 나무 자르기
+// 2110 - 공유기 설치
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(tokenizer.nextToken());
-        int M = Integer.parseInt(tokenizer.nextToken());
+        int C = Integer.parseInt(tokenizer.nextToken());
         int[] arr = new int[N];
-        int end = 0;
-        tokenizer = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(tokenizer.nextToken());
-            end = Math.max(arr[i], end);
+            arr[i] = Integer.parseInt(br.readLine());
         }
         br.close();
-        int start = 1;
+        Arrays.sort(arr);
+        int end = arr[N-1]; // 최대값은 마지막 집의 좌표
+        int start = 1; // 최소값은 1
         while (start <= end) {
             int mid = (start + end) / 2;
-            if(measureCutWood(arr, mid) >= M){
+            int count = 1;
+            int now = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if(arr[i] >= now + mid) {
+                    count++;
+                    now = arr[i];
+                }
+            }
+
+            if (count >= C) { // 설치한 공유기가 주어진 공유기 보다 많다 -> 거리 늘려야함
                 start = mid + 1;
-            } else {
+            } else { // 설치한 공유기가 주어진 공유기 보다 적다 -> 거리 줄여야함
                 end = mid - 1;
             }
         }
+
         bw.write(String.valueOf(end));
         bw.flush();
         bw.close();
     }
 
-    /**
-     *
-     * 나무 높이의 합을 계산하는 과정에서 int로는 모자람. long 타입 사용
-     *
-     */
-    static long measureCutWood(int[] arr, int length) {
-        long totalLength = 0;
-        for (int l : arr) {
-            if(l - length > 0) totalLength += (l - length);
-        }
-        return totalLength;
-    }
 }
