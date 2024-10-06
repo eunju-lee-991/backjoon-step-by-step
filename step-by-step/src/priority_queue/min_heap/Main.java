478a7ef915b26338e4b5516c5c432458a349f034
eunju <eunju7757@kai-i.com>
2024-10-06 21:38:39 +0900

    solve 11279 - max heap

---
+++ step-by-step/src/priority_queue/min_heap/Main.java
@@ -0,0 +1,27 @@
+package priority_queue.max_heap;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a );
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n > 0) {
+                pq.add(n);
+            } else {
+                int result = pq.isEmpty() ? 0 : pq.poll();
+                bw.write(result + "\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
