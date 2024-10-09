04912673345d798d0e351f4adf61bd921defc61d
eunju <eunju7757@kai-i.com>
2024-10-10 08:18:38 +0900

    solve 1927 - min heap

--- step-by-step/src/priority_queue/absolute_value_heap/Main.java
+++ step-by-step/src/priority_queue/absolute_value_heap/Main.java
@@ -1,9 +1,9 @@
-package priority_queue.min_heap;
+package priority_queue.max_heap;
 
 import java.io.*;
 import java.util.PriorityQueue;
 
-// 1927 - 최소 힙
+// 11279 - 최대 힙
 public class Main {
     public static void main(String[] args) throws IOException {
 
@@ -11,7 +11,7 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a );
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(br.readLine());
             if (n > 0) {
