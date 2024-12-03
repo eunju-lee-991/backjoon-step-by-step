d3398d05d558f9f46599d02c87087f96dd3bbac5
eunju <eunju7757@kai-i.com>
2024-12-03 23:12:27 +0900

    solve 1012 - organic cabbage

--- step-by-step/src/graph_and_traverse/organic_cabbage/Main.java
+++ step-by-step/src/graph_and_traverse/organic_cabbage/Main.java
@@ -1,4 +1,4 @@
-package graph_and_traverse.virus;
+package graph_and_traverse.organic_cabbage;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,54 +6,63 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.*;
 
-// 2606 - 바이러스
+// 1012 - 유기농 배추
 public class Main {
     static BufferedWriter bw ;
-    static boolean[] visited;
-    static List<Set<Integer>> nodes;
-
+    static int[][] farm;
+    static int count;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        visited = new boolean[N + 1];
-
-        nodes = new ArrayList<>();
-        nodes.add(new HashSet<>());
-        for (int i = 0; i < N; i++) {
-            nodes.add(new HashSet<>());
-        }
         StringTokenizer st;
-        for (int i = 0; i < M; i++) {
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            count = 0;
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken()); // 가로 길이
+            int N = Integer.parseInt(st.nextToken()); // 세로 길이
+            int K = Integer.parseInt(st.nextToken()); // 배추 위치의 개수
+            farm = new int[M][N];
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                farm[x][y] = 1;
+            }
+            for (int j = 0; j < M; j++) {
+                for (int k = 0; k < N; k++) {
 
-            nodes.get(a).add(b);
-            nodes.get(b).add(a);
+                    if (farm[j][k] == 1) {
+                        count++;
+                        dfs(j, k);
+                    }
+
+                }
+            }
+
+            bw.write(count+"\n");
         }
 
-        dfs(1);
-        int result = 0;
-        for (int i = 2; i < visited.length; i++) {
-            if(visited[i]) result++;
-        }
-
-        bw.write(result+"");
         bw.flush();
         bw.close();
     }
 
-    private static void dfs(int i) {
-        visited[i] = true;
-
-        for (int node : nodes.get(i)) {
-            if (!visited[node]) {
-                dfs(node);
+    static void dfs(int x, int y) {
+        if (isValid(x, y) && farm[x][y] == 1) {
+            farm[x][y] = 0;
+            for (int i = 0; i < 4; i++) {
+                dfs(x + dx[i], y + dy[i]);
             }
         }
+    }
+
+    static boolean isValid(int x, int y) {
+        if (x < 0 || x >= farm.length || y < 0 || y >= farm[0].length) {
+            return false;
+        }
+        return true;
     }
 }
