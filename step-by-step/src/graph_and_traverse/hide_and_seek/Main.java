5e4849b5d41ec8e56e8c21bcc4a3cba9f1ffe713
eunju <eunju7757@kai-i.com>
2024-12-07 21:06:04 +0900

    solve 2178 - maze

---
+++ step-by-step/src/graph_and_traverse/hide_and_seek/Main.java
@@ -0,0 +1,31 @@
+package graph_and_traverse.seek_and_hide;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 1697 - 숨바꼭질 
+public class Main {
+    static BufferedWriter bw;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+
+        bw.flush();
+        bw.close();
+    }
+
+}
--- step-by-step/src/graph_and_traverse/maze/Main.java
+++ step-by-step/src/graph_and_traverse/maze/Main.java
@@ -1,2 +1,61 @@
-package graph_and_traverse.maze;public class Main {
+package graph_and_traverse.maze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 2178 - 미로 탐색
+public class Main {
+    static BufferedWriter bw;
+    static int[][] maze;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        maze = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = line.charAt(j) - '0';
+            }
+        }
+        br.close();
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        while (!queue.isEmpty()) {
+            int[] xy = queue.poll();
+            int x = xy[0];
+            int y = xy[1];
+
+            if (x == N - 1 && y == M - 1) {
+                bw.write(maze[x][y] + "");
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (isValid(nx, ny) && maze[nx][ny] == 1) {
+                    queue.offer(new int[]{nx, ny});
+                    maze[nx][ny] = maze[x][y] + 1;
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static boolean isValid(int x, int y) {
+        return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length;
+    }
 }
