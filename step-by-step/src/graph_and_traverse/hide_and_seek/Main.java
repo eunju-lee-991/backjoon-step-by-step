0565b0b364b00eed98fef1cfc63a1515115cb3cc
eunju <eunju7757@kai-i.com>
2024-12-08 11:00:10 +0900

    1697 - hide and seek

--- step-by-step/src/graph_and_traverse/hide_and_seek/Main.java
+++ step-by-step/src/graph_and_traverse/hide_and_seek/Main.java
@@ -1,4 +1,4 @@
-package graph_and_traverse.seek_and_hide;
+package graph_and_traverse.hide_and_seek;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -8,11 +8,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 1697 - 숨바꼭질 
+// 1697 - 숨바꼭질
 public class Main {
     static BufferedWriter bw;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int max = 100000;
+    static boolean[] visited = new boolean[max + 1]; // max값과 VISITED로 범위 좁혀주는 것 중요!
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,12 +20,38 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
         br.close();
+        int second = 0;
 
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
+        visited[N] = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer q = queue.poll();
+                if (q == K) {
+                    bw.write(second + "");
+                    bw.flush();
+                    bw.close();
+                    return;
+                }
 
-        bw.flush();
-        bw.close();
+                if (q + 1 <= max && !visited[q + 1]) {
+                    queue.offer(q + 1);
+                    visited[q+1] = true;
+                }
+                if (q - 1 >= 0 && !visited[q - 1]) {
+                    queue.offer(q - 1);
+                    visited[q-1] = true;
+                }
+                if (q * 2 <= max && !visited[q * 2]) {
+                    queue.offer(q * 2);
+                    visited[q*2] = true;
+                }
+            }
+            second++;
+        }
+
     }
-
 }
