647b6684dcbbf604dd68d6f419445c2ed09f121a
eunju <eunju7757@kai-i.com>
2025-01-03 14:24:17 +0900

    solve 11050 - binomial coefficient 1

--- step-by-step/src/combinatorics/binomial_coefficient_1/Main.java
+++ step-by-step/src/combinatorics/binomial_coefficient_1/Main.java
@@ -1,25 +1,40 @@
-package combinatorics.factorial;
+package combinatorics.binomial_coefficient_1;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
+// 11050 - 이항 계수 1
 public class Main {
-    static int N;
+    static int[] factorials;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
-        int answer = 1;
-        if (N > 1) {
-            for (int i = 2; i <= N; i++) {
-                answer *= i;
-            }
-        }
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int K = Integer.parseInt(tokenizer.nextToken());
+        factorials = new int[N+1];
 
-        bw.write(answer+"");
+        //    n!
+        // --------
+        // r!(n-r)!
+
+        int answer = getFactorial(N) / (getFactorial(K) * getFactorial(N - K));
+        bw.write(answer + "");
         bw.flush();
         bw.close();
+    }
+
+    static int getFactorial(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i <= 1) {
+                factorials[i] = 1;
+            }
+            if (factorials[i] == 0) {
+                factorials[i] = factorials[i-1] * i;
+            }
+        }
+        return factorials[n];
     }
 }
