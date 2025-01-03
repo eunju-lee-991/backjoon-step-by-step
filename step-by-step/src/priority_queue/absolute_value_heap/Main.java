83cdc3f24964588604a710e2dc7b0797837323e6
eunju <eunju7757@kai-i.com>
2025-01-03 15:54:53 +0900

    11286 - absolute value heap

--- step-by-step/src/priority_queue/absolute_value_heap/Main.java
+++ step-by-step/src/priority_queue/absolute_value_heap/Main.java
@@ -1,20 +1,19 @@
-package priority_queue.max_heap;
+package priority_queue.absolute_value_heap;
 
 import java.io.*;
 import java.util.PriorityQueue;
 
-// 11279 - 최대 힙
+// 11286 - 절댓값 힙
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a );
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Math.abs(a) == Math.abs(b) ? a - b : Math.abs(a) - Math.abs(b) );
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(br.readLine());
-            if (n > 0) {
+            if (n != 0) {
                 pq.add(n);
             } else {
                 int result = pq.isEmpty() ? 0 : pq.poll();
