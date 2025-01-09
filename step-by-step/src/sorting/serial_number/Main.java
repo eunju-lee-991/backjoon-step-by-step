ee2825d41642c68a12236d6010f17bac5a8ad0ec
eunju <eunju7757@kai-i.com>
2025-01-09 11:10:28 +0900

    solve 1431 -  serial number

--- step-by-step/src/sorting/serial_number/Main.java
+++ step-by-step/src/sorting/serial_number/Main.java
@@ -1,2 +1,53 @@
-package sorting.serial_number;public class Main {
+package sorting.serial_number;
+
+import java.io.*;
+import java.util.Arrays;
+
+// 1431 - 시리얼 번호
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+        br.close();
+        Arrays.sort(arr, (a, b) -> {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            }
+
+            int sumA = 0;
+            int sumB = 0;
+            for (int i = 0; i < a.length(); i++) {
+                if(a.charAt(i) >= '0' && a.charAt(i) <= '9'){
+                    sumA += a.charAt(i) - '0';
+                }
+            }
+            for (int i = 0; i < b.length(); i++) {
+                if(b.charAt(i) >= '0' && b.charAt(i) <= '9'){
+                    sumB += b.charAt(i) - '0';
+                }
+            }
+            if(sumA != sumB) return sumA - sumB;
+
+            // 사전순
+            for (int i = 0; i < a.length(); i++) {
+                if(a.charAt(i) != b.charAt(i)) {
+                    return a.charAt(i) - b.charAt(i);
+                }
+            }
+
+            return 0;
+
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i]);
+            bw.write("\n");
+        }
+        bw.close();
+    }
 }
