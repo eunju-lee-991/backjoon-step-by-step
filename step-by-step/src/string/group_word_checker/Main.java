aaf17779de48cda0f883c8baa8f624e69f15afd8
eunju <eunju7757@kai-i.com>
2025-01-16 16:43:02 +0900

    solve 1316 - group word checker

--- step-by-step/src/string/group_word_checker/Main.java
+++ step-by-step/src/string/group_word_checker/Main.java
@@ -1,18 +1,37 @@
-package string.count_of_word;
+package string.group_word_checker;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-// 1152 - 단어의 개수
+// 1316 - 그룹 단어 체커
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = br.readLine().strip();
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            count += isGroupWord(input);
+        }
         br.close();
-        bw.write( input.equals("") ? "0" : input.split(" ").length + "");
+        bw.write(count + "");
         bw.close();
+    }
+
+    static int isGroupWord(String string) {
+        List<Character> list = new ArrayList<>();
+        list.add(string.charAt(0));
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i - 1) != string.charAt(i)) {
+                if (list.contains(string.charAt(i))) {
+                    return 0;
+                } else {
+                    list.add(string.charAt(i));
+                }
+            }
+        }
+        return 1;
     }
 }
