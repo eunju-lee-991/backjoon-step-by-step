03441e5071bf601e963d6447ccd93c7e67d0420d
eunju <eunju7757@gmail.com>
2025-03-02 09:38:15 +0900

    solve 이코테 다이나믹 프로그래밍 - 1로 만들기

--- step-by-step/src/dynamic_programming/make_it_1/Main.java
+++ step-by-step/src/dynamic_programming/make_it_1/Main.java
@@ -1,2 +1,23 @@
-package dynamic_programming.make_it_1;public class Main {
+package dynamic_programming.make_it_1;
+
+public class Main {
+    public static void main(String[] args) {
+        int X = 26;
+        int[] dp = new int[X + 1];
+
+        for (int i = 2; i <= X; i++) {
+            dp[i] = dp[i-1] + 1;
+
+            if (i % 5 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 5] + 1);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+        }
+        System.out.println(dp[X]);
+    }
 }
